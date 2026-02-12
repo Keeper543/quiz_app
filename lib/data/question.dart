b9cc7986bec6d502d458dc6811b07f5d7856f26f
tammiamckenzie <mckenzietammia@gmail.com>
2026-02-12 11:40:13 -0500

    intaial questions

---
+++ lib/data/question.dart
@@ -0,0 +1,9 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions =[
+   QuizQuestion('What classification was Sung Jinwoo given by the System?', [
+    'Necromancer'//correct
+    
+    ,'Fighter','Mage','Hunter']
+   )
+]
